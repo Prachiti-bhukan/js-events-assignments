64de55e382e5a73d2e3229f27a8a0c65600f16de
Prachiti bhukan <prachitibhukan@gmail.com>
2021-05-26 15:47:09 +0530

    final commit

--- exercise4.js
+++ exercise4.js
@@ -1,4 +1,10 @@
 window.addEventListener('load', () => {
 
+    const inputField = document.querySelector('#inputext');
+    const numval = document.querySelector('#number');
+    inputField.addEventListener('input', event => {
+        numval.innerText= inputField.value.length;
+ 
+    });
 
 });
