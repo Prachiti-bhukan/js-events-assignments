1101a42b9b44933cb7e81fb6ac4bd5e73ef74816
Prachiti bhukan <prachitibhukan@gmail.com>
2021-05-26 14:10:26 +0530

    updated js

---
+++ exercise-2.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', () =>
+{
+    const inputField =document.querySelector('#inputText');
+    const increase= document.querySelector('#incr');
+    const decrease= document.querySelector('#decr');
+})
