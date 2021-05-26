9f6261b03686eb1993f29b74916aa01e5294cbf4
Prachiti bhukan <prachitibhukan@gmail.com>
2021-05-26 17:52:43 +0530

    commit

--- exercise5.js
+++ exercise5.js
@@ -1,5 +1,9 @@
 window.addEventListener('load', () => {
-    
-
-
+    const hexval= document.querySelector('#inputNum');
+    const changecol= document.querySelector('#colour');
+    hexval.addEventListener('input', event => {
+         
+        changecol.style.backgroundColor.value = hexval;
+        
+    });
 });
