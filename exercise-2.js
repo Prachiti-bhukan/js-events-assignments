010667e61dd6056417fd5cfabbc85b4bd346c698
Prachiti bhukan <prachitibhukan@gmail.com>
2021-05-26 14:49:41 +0530

    final commit

--- exercise-2.js
+++ exercise-2.js
@@ -1,6 +1,17 @@
 window.addEventListener('load', () =>
 {
-    const inputField =document.querySelector('#inputText');
+    const input2 =document.querySelector('#inputText');
     const increase= document.querySelector('#incr');
     const decrease= document.querySelector('#decr');
-})
+    let count = 0;
+    increase.addEventListener('click', (event) => {
+        count += 1;
+        input2.value = count;
+
+    });
+
+    decrease.addEventListener('click', (event) => {
+        count -= 1;
+        input2.value = count;
+    });
+});
